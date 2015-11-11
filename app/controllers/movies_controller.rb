b8de008a7a93e25efbce9d1ea27fefc9cc92daf9
jbunny13 <jnguyen72086@gmail.com>
2015-11-11 10:50:45 -0700

    Add controller action & view for show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,10 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    id = params[:id]  # Retrieve movie ID from URI route
+    @movie = Movie.find(id)  # Look up movie by unique ID
+    # Will render app/views/movies/show.html.haml by default
+  end
 end
